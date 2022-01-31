b73cb3ca37cbcf2a2cd22ac5883cd31c575eb303
dragonthakur2107 <hemanshsinghthakur@gmail.com>
2022-01-31 21:38:37 +0530

    COMPLETED LANDING PAGE CSS HTML

--- main/app.js
+++ main/app.js
@@ -3,10 +3,19 @@ const aboutTC = document.querySelector(`#about-text-container`)
 const btnHome = document.querySelector(`#btn-home`)
 const btnAbout = document.querySelector(`#btn-about`)
 
+aboutTC.style.display="none";
+btnHome.style.color="#00E1EF"
+mainTC.style.display="flex";
 
-
-btnAbout.addEventListener(`click`,()=>{
-    aboutTC.style.display="block"
-    btnAbout.style.color="#00E1EF"
-    mainTC.style.display="none"
-})
+btnAbout.addEventListener("click",()=>{
+    aboutTC.style.display="flex";
+    btnAbout.style.color="#00E1EF";
+    mainTC.style.display="none";
+    btnHome.style.color="white"
+});
+btnHome.addEventListener("click",()=>{
+    aboutTC.style.display="none";
+    btnAbout.style.color="white";
+    mainTC.style.display="flex";
+    btnHome.style.color="#00E1EF"
+});
