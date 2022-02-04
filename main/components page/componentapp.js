f47c99fa269d9432031f644afcdd9a1603f68462
dragonthakur2107 <hemanshsinghthakur@gmail.com>
2022-02-04 11:17:30 +0530

    completed components designing remains

--- main/components page/componentapp.js
+++ main/components page/componentapp.js
@@ -1,23 +1,23 @@
 // ALERT-JS
-// const btnShowAlert = document.querySelector("#btn-show-alert")
-// const boxAlert = document.querySelector("#alert-box")
-// const btnHideAlert = document.querySelector("#btn-hide-alert")
+const btnShowAlert = document.querySelector("#btn-show-alert")
+const boxAlert = document.querySelector("#alert-box")
+const btnHideAlert = document.querySelector("#btn-hide-alert")
 
-// btnHideAlert.style.display="none"
-// boxAlert.style.display="none"
+btnHideAlert.style.display="none"
+boxAlert.style.display="none"
 
-// btnShowAlert.addEventListener("click",()=>{
-//     boxAlert.style.display="flex"
-//     btnHideAlert.style.display="block"
-//     btnShowAlert.style.display="none"
-// }
-// )
-// btnHideAlert.addEventListener("click",()=>{
-//     boxAlert.style.display="none"
-//     btnShowAlert.style.display="block"
-//     btnHideAlert.style.display="none"
-// }
-// )
+btnShowAlert.addEventListener("click",()=>{
+    boxAlert.style.display="flex"
+    btnHideAlert.style.display="block"
+    btnShowAlert.style.display="none"
+}
+)
+btnHideAlert.addEventListener("click",()=>{
+    boxAlert.style.display="none"
+    btnShowAlert.style.display="block"
+    btnHideAlert.style.display="none"
+}
+)
 
 //FLOATING-BUTTON
 // const btnShowButton = document.querySelector("#btn-show-button")
